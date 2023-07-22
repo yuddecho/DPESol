c5c881f0a0130a952a1a5cadce5be54f1661c399
echo <yuddecho@gmail.com>
2023-07-22 16:26:21 +0800

    n

--- run.sh
+++ run.sh
@@ -1,6 +1,8 @@
 CURR_PATH="$(pwd)"
 export PATH="${CURR_PATH}:${PATH}"
-export PYTHONPATH="${CURR_PATH}:${PYTHONPATH}"
+
+parent_dir=$(dirname "$PWD")
+export PYTHONPATH="${parent_dir}:${PYTHONPATH}"
 
 #NUM_GPUS=4
 #python -m torch.distributed.launch --nproc_per_node=${NUM_GPUS} training.py
